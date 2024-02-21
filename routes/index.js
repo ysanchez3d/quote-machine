f6956f53cbf058154e2272c440e41b533abfeddf
Yandri Sanchez <ysanchez3d@gmail.com>
2024-02-21 05:26:48 -0600

    delete unused code

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const axios = require("axios");
-
-
 const API = "https://api.api-ninjas.com/v1/quotes?";
-
-
-let favoriteQuotes = [
-  {
-    quote:
-      "There are two ways of spreading light: to be the candle or the mirror that reflects it.",
-    author: "Edith Wharton",
-    category: "inspirational",
-  },
-  {
-    quote:
-      "There are two ways of spreading light: to be the candle or the mirror that reflects it.",
-    author: "Edith Wharton",
-    category: "inspirational",
-  }
-];
 
 const getNewQuote = async function(category) {
   const response = await axios.get(API + `category=${category}`, {
